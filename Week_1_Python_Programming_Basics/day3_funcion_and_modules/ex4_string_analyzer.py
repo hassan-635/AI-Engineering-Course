3cd7013cf37aa471dc167be7c057c7b0c6c017cb
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2026-07-02 17:29:19 +0500

    used string operations in code

---
+++ Week_1_Python_Programming_Basics/day3_funcion_and_modules/ex4_string_analyzer.py
@@ -0,0 +1,8 @@
+
+from stringops import rev, vowel_count, palindrome_checker
+
+string = input("Enter a String : ")
+
+print(f"Reverse : {rev(string)}");
+print(f"Vowel Count : {vowel_count(string)}")
+print(f"Palindrome Check : {palindrome_checker(string)}")
