f16923dbd6ac7a75c4aebdfa1f8721c45ec7b09e
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2026-07-02 15:52:54 +0500

    renamed a folder and done exercise 1 od day 3: factorial functio

---
+++ Week_1_Python_Programming_Basics/day3_funcion_and_modules/EX1_FUNCTION_FACTORIAL.py
@@ -0,0 +1,15 @@
+
+
+def fact(num):
+    if num < 0:
+        return "Please Enter positive number"
+    else:
+        result = 1
+        for i in range(1, num+1):
+            result *= i
+        return result
+    
+print(fact(-3));
+print(fact(0));
+print(fact(1));
+print(fact(5));
