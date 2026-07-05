871d3fb9fec14d87e7788e99f2d14f17170164a5
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2026-07-05 11:34:47 +0500

    reverse words not letters

---
+++ Week_1_Python_Programming_Basics/day5_working_with_strings/word_revere.py
@@ -0,0 +1,8 @@
+
+text = "Artificial Intelligence is a very beneficial technology"
+
+words = text.split()
+reverse_words = words[::-1]
+
+reversed_text = " ".join(reverse_words)
+print(reversed_text)
