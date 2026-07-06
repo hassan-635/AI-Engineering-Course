d8bfdafd6a1e4905499cce43527061a1d77b26be
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2026-07-06 10:55:56 +0500

    reduce function

--- Week_1_Python_Programming_Basics/day7_pythonic_code/lambda_function.py
+++ Week_1_Python_Programming_Basics/day7_pythonic_code/lambda_function.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 
 add = lambda x, y: (x+y)
 print(add(3, 5))
@@ -7,5 +9,10 @@ num = [1, 2, 3, 4]
 squares = map(lambda x: x**2, num)
 print(list(squares))
 
+
 even = filter(lambda x: x%2 == 0, num)
 print(list(even))
+
+
+product = reduce(lambda x, y: x*y, num)
+print(product)
