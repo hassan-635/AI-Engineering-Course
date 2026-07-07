57bd8261c9fec18ffd3f0785a63d7d5ad36230fa
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2026-07-07 21:53:48 +0500

    sum of rows and columns of a multi dimensional array

--- Week_2_Data_Science_Essentials/day1_numpy/sum_of_rows_and_cols.py
+++ Week_2_Data_Science_Essentials/day1_numpy/sum_of_rows_and_cols.py
@@ -1,7 +1,11 @@
 
 import numpy as np
 
-matrix = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [8, 7, 6, 5], [4, 3, 2, 1]])
+matrix = np.array([[1, 2, 3, 4],
+                   [5, 6, 7, 8], 
+                   [8, 7, 6, 5], 
+                   [4, 3, 2, 1]])
+
 matrix2 = matrix.T
 print(matrix)
 
@@ -9,4 +13,14 @@ for i in range(0, 4):
     r_sum = np.sum(matrix[i])
     c_sum = np.sum(matrix2[i])
     print(f"{i+1}th row : {r_sum}")
+    print(f"{i+1}th column : {c_sum}")
+
+
+print("___________________________________________________________")
+
+
+for i in range(0, 4):
+    c_sum = 0
+    for j in range(0, 4):
+        c_sum += matrix[j][i]
     print(f"{i+1}th column : {c_sum}")
