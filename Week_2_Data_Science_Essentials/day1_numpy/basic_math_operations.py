5ab4b60cf163015d9a0487d598aafebc0d1ec457
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2026-07-07 20:26:14 +0500

    basic math operation

---
+++ Week_2_Data_Science_Essentials/day1_numpy/basic_math_operations.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+arr = np.arange(1, 6)
+brr = np.arange(6, 11)
+
+print(f"Array 1 : {arr}")
+print(f"Array 2 : {brr}")
+
+print(f"Division : {arr/brr}")
+print(f"Multiplication : {arr*brr}")
+print(f"Addition : {arr+brr}")
+print(f"Subtraaction : {arr-brr}")
