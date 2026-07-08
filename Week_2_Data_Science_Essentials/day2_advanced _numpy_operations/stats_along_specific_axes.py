5d8eaeec7676544b3c5a83734216a41a3938d21b
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2026-07-08 17:21:36 +0500

    statistics along specific axis

--- Week_2_Data_Science_Essentials/day2_advanced _numpy_operations/stats_along_specific_axes.py
+++ Week_2_Data_Science_Essentials/day2_advanced _numpy_operations/stats_along_specific_axes.py
@@ -8,3 +8,9 @@ print(matrix)
 
 print(f"Sum along col : {np.sum(matrix, axis = 0)}")
 print(f"Sum along row : {np.sum(matrix, axis = 1)}")
+
+print(f"Mean along col : {np.mean(matrix, axis = 0)}")
+print(f"Mean along row : {np.mean(matrix, axis = 1)}")
+
+print(f"Std along col : {np.std(matrix, axis = 0)}")
+print(f"Std along row : {np.std(matrix, axis = 1)}")
