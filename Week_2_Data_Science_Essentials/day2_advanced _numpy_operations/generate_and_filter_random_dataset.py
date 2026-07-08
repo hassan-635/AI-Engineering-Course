657e287eccba7824f7718ebd8e36859edd34c024
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2026-07-08 16:36:13 +0500

    filteration of dataset

--- Week_2_Data_Science_Essentials/day2_advanced _numpy_operations/generate_and_filter_random_dataset.py
+++ Week_2_Data_Science_Essentials/day2_advanced _numpy_operations/generate_and_filter_random_dataset.py
@@ -1,7 +1,11 @@
 
 import numpy as np
 
-ds = np.random.randint(1, 50, size=(5, 5))
+ds = np.random.randint(1, 51, size=(5, 5))
 
 print("Dataset : ")
+print(ds)
+
+ds[ds > 25] = 0
+print("Filtered Dataset : ")
 print(ds)
