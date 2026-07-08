68bbc6a7129c56e4fe3df20189d80b1cc065ad02
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2026-07-08 16:39:11 +0500

    seeding and statistics calculation of dataset

--- Week_2_Data_Science_Essentials/day2_advanced _numpy_operations/generate_and_filter_random_dataset.py
+++ Week_2_Data_Science_Essentials/day2_advanced _numpy_operations/generate_and_filter_random_dataset.py
@@ -1,6 +1,7 @@
 
 import numpy as np
 
+np.random.seed(42)
 ds = np.random.randint(1, 51, size=(5, 5))
 
 print("Dataset : ")
@@ -9,3 +10,7 @@ print(ds)
 ds[ds > 25] = 0
 print("Filtered Dataset : ")
 print(ds)
+
+print(f"Sum : {np.sum(ds)}")
+print(f"Mean : {np.mean(ds)}")
+print(f"Deviation : {np.std(ds)}")
