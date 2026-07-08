7f03c27f1300e2260672bba9775d0c5b107dc255
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2026-07-08 17:16:55 +0500

    sum along specific axis

--- Week_2_Data_Science_Essentials/day2_advanced _numpy_operations/stats_along_specific_axes.py
+++ Week_2_Data_Science_Essentials/day2_advanced _numpy_operations/stats_along_specific_axes.py
@@ -6,3 +6,5 @@ matrix = np.random.randint(0, 51, size=(3, 3))
 print("Matrix : ")
 print(matrix)
 
+print(f"Sum along col : {np.sum(matrix, axis = 0)}")
+print(f"Sum along row : {np.sum(matrix, axis = 1)}")
