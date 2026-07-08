e5c3ec78d3e94f3f16a3dd3c75b5be40d7407d5c
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2026-07-08 16:27:10 +0500

    broadcasting a matrix

---
+++ Week_2_Data_Science_Essentials/day2_advanced _numpy_operations/broadcasting.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+matrix = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+print(matrix)
+
+vector = [1, 1, 1]
+
+broadcasted_matrix = matrix + vector;
+
+print("Boradcasted matrix : ")
+print(broadcasted_matrix)
+
