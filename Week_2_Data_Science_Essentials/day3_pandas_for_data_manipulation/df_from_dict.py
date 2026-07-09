12cd15b639b3a8f69c4c20f39ea26a51dabe6013
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2026-07-09 10:41:56 +0500

    dictionary to datarame and addition of column in dataframe

---
+++ Week_2_Data_Science_Essentials/day3_pandas_for_data_manipulation/df_from_dict.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+student_data = {
+    "Name": ["Hassan", "Ali", "Ahmad", "Umer", "Bilal"],
+    "Age": [21, 22, 20, 23, 21],
+    "Department": ["CS", "AI", "SE", "CS", "AI"],
+    "CGPA": [3.45, 3.80, 3.20, 3.60, 3.90]
+}
+
+df = pd.DataFrame(student_data)
+print(df)
+
+df["Country"] = ["Pakistan", "Iran", "Iraq", "Pakistan", "Iran"]
+print(df)
