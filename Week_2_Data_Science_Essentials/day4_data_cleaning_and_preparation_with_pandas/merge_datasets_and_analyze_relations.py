5481032c96460e7c06f1469b608ba89a3b325a03
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2026-07-10 20:54:17 +0500

    added dictionaries

---
+++ Week_2_Data_Science_Essentials/day4_data_cleaning_and_preparation_with_pandas/merge_datasets_and_analyze_relations.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+students = {
+    "Student_ID": [101, 102, 103, 104, 105],
+    "Name": ["Hassan", "Ali", "Ahmad", "Bilal", "Umer"]
+}
+
+marks = {
+    "Student_ID": [101, 102, 103, 104, 105],
+    "Math": [90, 75, 80, 65, 95],
+    "Physics": [88, 70, 82, 60, 98]
+}
+
+attendance = {
+    "Student_ID": [101, 102, 103, 104, 105],
+    "Attendance": [95, 80, 90, 70, 98]
+}
