4eb33d1ef7d86a6ff6c642ba451a6384b54e68ae
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2026-07-10 12:35:10 +0500

    missing valued dict to dataframe

---
+++ Week_2_Data_Science_Essentials/day4_data_cleaning_and_preparation_with_pandas/fill_missing_vals_and_rename_cols.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+
+data = {
+    "Name": ["Hassan", "Ali", np.nan, "Uzair", "Ahmed", np.nan],
+    "Age" : [22, np.nan, 23, 21, np.nan, 50],
+    "Score" : [90, 50, np.nan, np.nan, 10, np.nan]
+}
+
+df = pd.DataFrame(data)
+print(df)
