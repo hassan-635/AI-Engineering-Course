d234a4667d16d4cd79b2da8b6a562b40b3ff4956
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2026-07-10 19:33:04 +0500

    datasets

--- Week_2_Data_Science_Essentials/day4_data_cleaning_and_preparation_with_pandas/merge_and_transform_two_datasets.py
+++ Week_2_Data_Science_Essentials/day4_data_cleaning_and_preparation_with_pandas/merge_and_transform_two_datasets.py
@@ -1,3 +1,13 @@
 import pandas as pd
 import numpy as np
 
+df1 = pd.DataFrame({
+    "id": [1, 2, 3],
+    "Name" : ["Hassan", "Ali", "Abrar"],
+    "Age" : [19, 15, 18]
+})
+
+df2 = pd.DataFrame({
+    "id" : [2, 3, 4],
+    "Country" : ["Pakistan", "Iran", "Iraq"]
+})
