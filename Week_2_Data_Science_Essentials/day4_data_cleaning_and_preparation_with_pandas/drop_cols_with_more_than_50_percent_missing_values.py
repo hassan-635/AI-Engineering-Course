5a93d0810327c43ab20c162d426df034260a9455
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2026-07-10 20:42:51 +0500

    dastaframe creation

---
+++ Week_2_Data_Science_Essentials/day4_data_cleaning_and_preparation_with_pandas/drop_cols_with_more_than_50_percent_missing_values.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+df = pd.DataFrame({
+    "Id" : [1, 2, 3, 4, 5, 6, 7, 8],
+    "Name" : ["Alice", "Bob", np.nan, np.nan, "Elon", "Faizan", np.nan, "Haider"],
+    "Marks" : [45, 50, 56, np.nan, np.nan, np.nan, np.nan, np.nan], 
+    "Score" : [10, 20, np.nan, np.nan, np.nan, np.nan, np.nan, 10]
+})
+
+print("\n_____________________________________________________________\nOriginal Dataset : \n")
+print(df.to_string(index=False))
+print("\n_____________________________________________________________\nDropped Miising Values Dataset : \n")
