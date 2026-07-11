67992896f6142dba71030426bc1f216e584281ed
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2026-07-11 15:32:23 +0500

    sales data

---
+++ Week_2_Data_Science_Essentials/day5_data_aggregation_and_grouping_in_pandas/sales_data.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+sales_data = {
+    "Order_ID": [101,102,103,104,105,106,107,108,109,110,111,112],
+    "Salesperson": [
+        "Ali", "Hassan", "Ali", "Ahmed",
+        "Hassan", "Ali", "Bilal", "Ahmed",
+        "Bilal", "Hassan", "Ali", "Ahmed"
+    ],
+    "City": [
+        "Lahore", "Karachi", "Lahore", "Islamabad",
+        "Karachi", "Lahore", "Islamabad", "Lahore",
+        "Karachi", "Islamabad", "Karachi", "Lahore"
+    ],
+    "Product": [
+        "Laptop", "Mobile", "Tablet", "Laptop",
+        "Laptop", "Mobile", "Tablet", "Mobile",
+        "Laptop", "Tablet", "Laptop", "Mobile"
+    ],
+    "Quantity": [2, 5, 3, 1, 4, 2, 6, 3, 2, 5, 1, 4],
+    "Price": [120000, 50000, 70000, 120000, 120000, 50000, 70000, 50000, 120000, 70000, 120000, 50000]
+}
+
+df = pd.DataFrame(sales_data)
+
+# Total Sale Amount
+df["Total_Sale"] = df["Quantity"] * df["Price"]
+
+print(df)
