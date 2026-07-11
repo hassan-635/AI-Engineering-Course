2df19e619df46744e951ad604ce7610fb89d4193
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2026-07-11 09:54:41 +0500

    dictionary to dataframe

---
+++ Week_2_Data_Science_Essentials/day4_data_cleaning_and_preparation_with_pandas/one_hot_encoding.py
@@ -0,0 +1,25 @@
+import pandas as pd
+student_data = {
+    "Name": [
+        "Hassan", "Ali", "Muneeb", "Bilal", "Umer",
+        "Ahsan", "Uzair", "Hamza", "Saad", "Zain"
+    ],
+    "Department": [
+        "AI", "CS", "SE", "AI", "CS",
+        "IT", "SE", "AI", "IT", "CS"
+    ],
+    "Gender": [
+        "Male", "Male", "Male", "Male", "Male",
+        "Female", "Female", "Male", "Male", "Male"
+    ],
+    "City": [
+        "Rawalpindi", "Lahore", "Islamabad", "Karachi", "Peshawar",
+        "Rawalpindi", "Lahore", "Karachi", "Islamabad", "Peshawar"
+    ],
+    "CGPA": [
+        3.45, 3.80, 3.20, 3.60, 3.90,
+        3.75, 3.50, 3.10, 3.65, 3.85
+    ]
+}
+df = pd.DataFrame(student_data)
+print(df)
