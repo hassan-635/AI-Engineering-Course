430e6c639059bfcc64364685c7d854fa9bc964e5
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2026-07-11 13:32:36 +0500

    Categorical Data Dictionary to dataframe

---
+++ Week_2_Data_Science_Essentials/day5_data_aggregation_and_grouping_in_pandas/load_categorical_data_and_group_it.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+data = {
+    "Class" : ["A", "B", "A", "B", "C", "C"],
+    "Score" : [85, 90, 88, 72, 95, 80],
+    "Age" : [15, 16, 15, 17, 16, 15]
+}
+
+df = pd. DataFrame(data)
+print("Original Data : ")
+print(df.to_string(index=False))
