1c1f7719f330caefef0c80b74fbfed7d0e6993ee
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2026-07-12 10:06:25 +0500

    gym dataset loaded

--- Week_2_Data_Science_Essentials/day6_data_visualization_using_matpltlib_and_seaborn/Fit_life_gym_membership_analysis.py
+++ Week_2_Data_Science_Essentials/day6_data_visualization_using_matpltlib_and_seaborn/Fit_life_gym_membership_analysis.py
@@ -1,1 +1,45 @@
-# line graph, bar chart, scatter plot
+import pandas as pd
+
+gym = {
+    "Member_ID":[101,102,103,104,105,106,107,108,109,110,111,112],
+
+    "Month":[
+        "Jan","Feb","Mar","Apr","May","Jun",
+        "Jul","Aug","Sep","Oct","Nov","Dec"
+    ],
+
+    "New_Members":[
+        45,52,60,70,85,90,
+        88,95,80,75,68,72
+    ],
+
+    "Trainer":[
+        "Ali","Ahmed","Ali","Hassan","Ahmed","Ali",
+        "Hassan","Ali","Ahmed","Ali","Hassan","Ahmed"
+    ],
+
+    "Workout_Hours":[
+        5,7,8,6,9,10,
+        8,11,7,6,9,10
+    ],
+
+    "Calories_Burned":[
+        1200,1700,2000,1500,2400,2600,
+        2100,2800,1800,1600,2350,2550
+    ],
+
+    "Age":[
+        18,20,22,25,24,27,
+        30,28,26,21,23,29
+    ],
+
+    "City":[
+        "Lahore","Karachi","Islamabad","Lahore",
+        "Karachi","Islamabad","Lahore",
+        "Karachi","Islamabad","Lahore",
+        "Karachi","Islamabad"
+    ]
+}
+
+df = pd.DataFrame(gym)
+print(df)
