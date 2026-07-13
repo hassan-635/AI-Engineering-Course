b953b1f5632aee803c16b45d9cee5533e1efadff
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2026-07-13 13:16:22 +0500

    dataset

---
+++ Week_2_Data_Science_Essentials/day6_data_visualization_using_matpltlib_and_seaborn/multiple_histograms_overlaid.py
@@ -0,0 +1,35 @@
+# gym meber analysis
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+gym = {
+    "Member_ID": [101,102,103,104,105,106,107,108,109,110,111,112,113,114,115],
+
+    "Trainer": [
+        "Ali","Ali","Ali","Ali","Ali",
+        "Ahmed","Ahmed","Ahmed","Ahmed","Ahmed",
+        "Hassan","Hassan","Hassan","Hassan","Hassan"
+    ],
+
+    "Workout_Hours": [
+        5,6,7,8,9,
+        7,8,9,10,11,
+        4,5,6,7,8
+    ],
+
+    "Calories_Burned": [
+        1500,1700,1900,2100,2300,
+        1800,2000,2200,2500,2700,
+        1200,1400,1600,1800,2000
+    ],
+
+    "Age": [
+        20,21,22,23,24,
+        25,26,27,28,29,
+        19,20,21,22,23
+    ]
+}
+
+df = pd.DataFrame(gym)
+print(df.to_string(index=False))
