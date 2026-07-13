a575c18f2476b2b0ab15d48c815b5de746f771bb
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2026-07-13 10:57:03 +0500

    student dataset

---
+++ Week_2_Data_Science_Essentials/day6_data_visualization_using_matpltlib_and_seaborn/student_performance_analysis.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+students = {
+    "Student": [
+        "Ali","Ahmed","Hassan","Bilal","Umer",
+        "Saad","Usman","Hamza","Ahsan","Zain"
+    ],
+
+    "Math": [90,75,85,60,95,70,88,65,80,92],
+
+    "Physics": [88,70,82,58,96,68,90,62,78,94],
+
+    "Chemistry": [85,72,80,65,90,70,84,68,76,89],
+
+    "English": [70,80,75,85,68,90,72,88,84,71],
+
+    "Computer": [95,78,90,70,98,75,94,72,86,97]
+}
+
+df = pd.DataFrame(students)
+print(df.to_string(index=False))
